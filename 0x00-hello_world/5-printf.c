93de51762022eb0fa17372f3d7b34f53fd55c0cb
chalag17 <chalagirma17@gmail.com>
2022-04-07 09:24:16 -0700

    b

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -3,9 +3,9 @@
  * main- the main function
  *my hello. printf task
  * Return: 0 after printing your function
- */
+*/
 int main(void)
 {
-   printf("with proper grammar, but the outcome is a piece of art,\n");
-  return (0); 
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0); 
 }
