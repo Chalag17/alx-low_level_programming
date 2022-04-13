45b66a5bbc58cf969e1b731d6e7003354034d4ee
chalag17 <chalagirma17@gmail.com>
2022-04-13 04:49:07 -0700

    task 15

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -12,11 +12,11 @@
 
 int main(void)
 {
-  unsigned long fib1 = 0, fib2 = 1, sum;
+unsigned long fib1 = 0, fib2 = 1, sum;
 float total_sum;
 
 while (1)
-    {
+{
 sum = fib1 + fib2;
 
 if (sum > 4000000)
