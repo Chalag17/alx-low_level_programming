d7ad6ceb5b6d5f9c481d8082360f2bf03809907a
chalag17 <chalagirma17@gmail.com>
2022-04-13 04:53:30 -0700

    task 16

---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,35 @@
+#include"main.h"
+#include<studio>
+/**
+ * numLength - returns the length of string
+ *
+ * @num: operand number
+ *
+ * Return: number of digits
+ */
+
+int numLength(int num)
+{
+int length = 0;
+
+if (!num)
+return (1);
+
+while (num)
+{
+num = num / 10;
+length += 1;
+}
+
+return (length);
+}
+
+/**
+ * main - Entry point
+ *
+ * Description: prints the first 98 Fibonacci numbers
+ *starting with 1 and 2 followed by a new line
+ *
+ *Solution was copied from Nobert Patrick
+ *Wise, github handle: Trikcode
+ *
