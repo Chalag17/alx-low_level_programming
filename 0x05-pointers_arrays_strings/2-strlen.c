5eb82eb9e0ef4ac6fd2b488d31c189d258d9f98f
chalag17 <chalagirma17@gmail.com>
2022-04-19 01:03:34 -0700

    task 2

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -2,9 +2,8 @@
 
 /**
  * _strlen -length of string 
- * @s: A pointer prints a string, followed by a newline to stdout
+ *@s: A pointer prints a string, followed by a newline to stdout
  * 
- * @str: string parameter to print
  *
  * Return:void
  */
